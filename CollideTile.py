f822a7856f4c60ca7a69a385209191602b7e4129
oryozana <oryozana@gmail.com>
2022-03-13 09:46:05 +0200

    abc

--- CollideTile.py
+++ CollideTile.py
@@ -4,9 +4,12 @@ from Functions import *
 from BasicTile import BasicTile
 
 
-class CollideTile(BasicTile):
+class CollideTile(Tile):
     def __init__(self, img_src, x, y):
         super().__init__(img_src, x, y)
 
     def isWalkable(self):
+        return False
+
+    def isKillable(self):
         return False
